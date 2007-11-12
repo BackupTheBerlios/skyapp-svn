c41cec07781a790cd241206c7e5f18775d657bf2
trinculescu <trinculescu@30d8964e-0c16-0410-a233-f86d1788202a>
2007-11-12 01:58:41 +0000

    touches

--- branches/0.4/app/pages/nested/phpinfo.php
+++ branches/0.4/app/pages/nested/phpinfo.php
@@ -1,11 +1,14 @@
 <?php
 class Page_phpinfo extends SA_Page {
 	public function init() {
-		$this->setLayout(null);
+		//$this->setLayout(null);
 		$this->setTemplate(null);
-	}
-	
-	public function display() {
-		phpinfo();	
+		
+		ob_start();
+		phpinfo();
+		$info = ob_get_contents();
+		ob_end_clean();
+		$info = '<p><a href="' . SA_Url::url('/default') . '">Back</a></p>' . $info;
+		$this->setContents(CONTENT_FOR_LAYOUT, $info);		
 	}
 }
